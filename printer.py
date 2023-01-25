10a3f4744f1d4fcc11c8468e118f1f672a4403da
paulonhantumbojr <paulonhantumbo12@gmail.com>
2023-01-25 17:07:45 +1100

    Finalised Initialisations

--- printer.py
+++ printer.py
@@ -1,8 +1,11 @@
+# Exception catcher
 import io
 import requests
 from datetime import datetime
 
-def toHTML(data, pages=None):
+def toHTML(data, pages=None): #data variable represents the elements in index.html 
+
+  # Initialise recommendations in the quest
   quest = [
     data['q_a1'], #0
     data['q_a2'], #1
@@ -10,15 +13,14 @@ def toHTML(data, pages=None):
     data['q_a4'], #3
     data['q_a5'], #4
     data['q_a6r'], #5
-    data['q_a6'], #6
+    "", #6 data['q_a6'],
     data['q_a7'], #7
     data['q_a8r'], #8
-    data['q_a8'], #9   
+    "", #9  data['q_a8']
     data['q_a9r'], #10
-    data['q_a9'], #11  
+    "", #11 data['q_a9']
     data['q_a10r'], #12
-    data['q_a10'], #13  
-    "", #Initialisation (14)
+    "", #13 data['q_a10']
   ]
 
   answ = [
@@ -40,22 +42,60 @@ def toHTML(data, pages=None):
   s3 = 10 if quest[2] == answ[2] else 0 
   s4 = 10 if quest[3] == answ[3] else 0
   s5 = 10 if quest[4] == answ[4] else 0 
-  s6 = 10 if quest[5] == answ[5] or quest[6] == answ[5] else 0 
 
-# If user gets the response correct in the first try initialize the recommendation as an empty string
-  if quest[5] == answ[5]: # If the user gets the question correctly
-    quest[6] == quest[14] # Initialise the recommendation
-    s6 = 10 
-    # data['q_a6'] = "" # Initialize data['q_a6'] as an empty variable. The issue is data['q_a6'] intrisically has a value associated to it, that is referenced in the html file. Therefore the value would need to be assigned in this script
-  elif quest[5] != answ[5] and quest[6] == answ[5]: # Else if the user gets the first prompt incorrectly and the recommendation right
+  # s6 = 10 if quest[5] == answ[5] or quest[6] == answ[5] else 0 
+  if quest[5] == answ[5]:
     s6 = 10
+    tick6 = True
+    select6r = True
+    if quest[5] != answ[5]:
+      quest[6] = data['q_a6']
+    elif quest[6] == answ[5]:
+      s6 = 10    
+      tick6 = True
   else:
-    s6 = 0 
+    s6 = 0
 
   s7 = 10 if quest[7] == answ[6] else 0 
-  s8 = 10 if quest[8] == answ[7] or quest[9] == answ[7] else 0
-  s9 = 10 if quest[10] == answ[8] or quest[11] == answ[8] else 0
-  s10 = 10 if quest[12] == answ[9] or quest[13] == answ[9] else 0
+
+  # s8 = 10 if quest[8] == answ[7] or quest[9] == answ[7] else 0
+  if quest[8] == answ[7]:
+    s8 = 10
+    tick8 = True
+    select8r = True
+    if quest[8] != answ[7]:
+      quest[9] = data['q_a8']
+    elif quest[9] == answ[7]:
+      s8 = 10 
+      tick8 = True   
+  else:
+    s8 = 0
+
+  # s9 = 10 if quest[10] == answ[8] or quest[11] == answ[8] else 0
+  if quest[10] == answ[8]:
+    s9 = 10
+    tick9 = True
+    select9r = True
+    if quest[10] != answ[8]:
+      quest[11] = data['q_a9']
+    elif quest[11] == answ[8]:
+      s9 = 10    
+      tick9 = True
+  else:
+    s9 = 0
+
+  # s10 = 10 if quest[12] == answ[9] or quest[13] == answ[9] else 0
+  if quest[12] == answ[9]:
+    s10 = 10
+    tick10 = True
+    select10r = True
+    if quest[12] != answ[9]:
+      quest[13] = data['q_a10']
+    elif quest[13] == answ[9]:
+      s10 = 10    
+      tick10 = True
+  else:
+    s10 = 0
 
   finsc = s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9 + s10
 
@@ -71,15 +111,24 @@ def toHTML(data, pages=None):
     'correct4': answ[3],
     'select5': quest[4], 
     'correct5': answ[4],
-    'select6': quest[5] if quest[5] == answ[5] else quest[6],
-    'correct6': answ[5],    
+
+    # 'select6': quest[5] if quest[5] == answ[5] else quest[6],
+    'select6': quest[5] if select6r == True else data['q_a6'],
+    'correct6': answ[5],  
+
     'select7': quest[7], 
     'correct7': answ[6],  
-    'select8': quest[8] if quest[8] == answ[7] else quest[9], 
+
+    # 'select8': quest[8] if quest[8] == answ[7] else quest[9],
+    'select8': quest[8] if select8r == True else data['q_a8'],
     'correct8': answ[7],    
-    'select9': quest[10] if quest[10] == answ[8] else quest[11], 
+
+    # 'select9': quest[10] if quest[10] == answ[8] else quest[11], 
+    'select9': quest[10] if select9r == True else data['q_a9'],
     'correct9': answ[8],    
-    'select10': quest[12] if quest[12] == answ[9] else quest[13], 
+
+    # 'select10': quest[12] if quest[12] == answ[9] else quest[13], 
+    'select10': quest[12] if select10r == True else data['q_a10'],
     'correct10': answ[9],    
 
     'tick1': '&#9989;' if quest[0] == answ[0] else '&#10060;',
@@ -87,11 +136,18 @@ def toHTML(data, pages=None):
     'tick3': '&#9989;' if quest[2] == answ[2] else '&#10060;',
     'tick4': '&#9989;' if quest[3] == answ[3] else '&#10060;',
     'tick5': '&#9989;' if quest[4] == answ[4] else '&#10060;',   
-    # 'tick6': '&#9989;' if quest[5] == answ[5] or quest[6] == answ[5] else '&#10060;',  # Account for initialisation
+
+    # 'tick6': '&#9989;' if quest[5] == answ[5] or quest[6] == answ[5] else '&#10060;',  
+    'tick6': '&#9989;' if tick6 == True else '&#10060;',
+
     'tick7': '&#9989;' if quest[7] == answ[6] else '&#10060;', 
-    'tick8': '&#9989;' if quest[8] == answ[7] or quest[9] == answ[7] else '&#10060;',  
-    'tick9': '&#9989;' if quest[10] == answ[8] or quest[11] == answ[8] else '&#10060;',  
-    'tick10': '&#9989;' if quest[12] == answ[9] or quest[13] == answ[9] else '&#10060;',  
+
+    # 'tick8': '&#9989;' if quest[8] == answ[7] or quest[9] == answ[7] else '&#10060;', 
+    'tick8': '&#9989;' if tick8 == True else '&#10060;',
+    # 'tick9': '&#9989;' if quest[10] == answ[8] or quest[11] == answ[8] else '&#10060;', 
+    'tick9': '&#9989;' if tick9 == True else '&#10060;',
+    # 'tick10': '&#9989;' if quest[12] == answ[9] or quest[13] == answ[9] else '&#10060;',  
+    'tick10': '&#9989;' if tick10 == True else '&#10060;',
   }
 
   with open('template.html', 'U') as f:
@@ -110,21 +166,21 @@ def send(html):
 
 if __name__ == '__main__':
   html = toHTML({
+    # Initialise the variables of the recommended questions
     "q_a1": "Basketball",
     "q_a2": "Shaping leaders",
     "q_a3": "A troop",
     "q_a4": "A Mechanic",
     "q_a5": "Brisbane", 
     "q_a6r": "14th", 
-    "q_a6": "11th",          
+    "": "", # "q_a6": "11th",       
     "q_a7": "Lachlan River", 
     "q_a8r": "Fear of needles", 
-    "q_a8": "Fear of heights",
+    "": "", # "q_a8": "Fear of heights"
     "q_a9r": "A banana", 
-    "q_a9": "An apple",
+    "": "", # "q_a9": "An apple",
     "q_a10r": "8", 
-    "q_a10": "10",
-    "":"", # Initialisation term
+    "": "", # "q_a10": "10"
   })
   
   with open('out.html', 'w') as f:
